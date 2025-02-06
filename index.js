48775c0d546df5b8d59a420fbe2a0fa06d0867bc
Evgeniy <evgeniipusser88@gmail.com>
2025-02-06 14:36:42 +0200

    special file for HW1 JS Lesson2

---
+++ index.js
@@ -0,0 +1,30 @@
+
+function sumOfDigits(str) {
+  
+  let sum = 0;
+  let num = +str; // Convert the string to a number once
+
+   if (isNaN(num)) {
+  console.log("Not a number");
+  return;
+}
+
+
+  while (num > 0) {
+    let digit = Math.trunc(num % 10); // Extract last digit
+    sum += digit;
+    num = Math.trunc(num / 10); // Remove the last digit
+  }
+
+  return sum;
+}
+
+
+function displayAnanas() {
+    console.log(('a' + (0/0) + 'as').toLowerCase());
+}
+
+
+
+console.log(sumOfDigits("8999890.99"))
+displayAnanas();
